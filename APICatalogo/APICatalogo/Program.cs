6e81cc87695fb7034991ae45cd79ee7860fb930d
WillianNalbert <williannalbert@hotmail.com>
2024-04-22 07:26:08 -0300

    implementação de versionamento

--- APICatalogo/APICatalogo/Program.cs
+++ APICatalogo/APICatalogo/Program.cs
@@ -7,6 +7,7 @@ using APICatalogo.Models;
 using APICatalogo.RateLimitOptions;
 using APICatalogo.Repositories;
 using APICatalogo.Services;
+using Asp.Versioning;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.RateLimiting;
@@ -155,6 +156,20 @@ builder.Services.AddRateLimiter(options =>
                                 QueueLimit = 0,
                                 Window = TimeSpan.FromSeconds(10)
                             }));
+});
+
+builder.Services.AddApiVersioning(o =>
+{
+    o.DefaultApiVersion = new ApiVersion(1, 0);
+    o.AssumeDefaultVersionWhenUnspecified = true;
+    o.ReportApiVersions = true;
+    //o.ApiVersionReader = ApiVersionReader.Combine(
+    //                      new QueryStringApiVersionReader(),
+    //                      new UrlSegmentApiVersionReader());
+}).AddApiExplorer(options =>
+{
+    options.GroupNameFormat = "'v'VVV";
+    options.SubstituteApiVersionInUrl = true;
 });
 
 //builder.Services.AddTransient<IMeuServico, MeuServico>();
