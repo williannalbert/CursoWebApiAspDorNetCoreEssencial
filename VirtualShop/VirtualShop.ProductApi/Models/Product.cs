e0ab7399383e2ad82ebadbcd72825f8e3735ef4c
WillianNalbert <williannalbert@hotmail.com>
2024-05-20 20:31:30 -0300

    inicio do projeto na loja com micro serviços

---
+++ VirtualShop/VirtualShop.ProductApi/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace VirtualShop.ProductApi.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public decimal? Price { get; set; }
+    public string? Description { get; set; }
+    public long Stock { get; set; }
+    public string? ImageURL { get; set; }
+    public Category? Category { get; set; }
+    public int CategoryId { get; set; }
+}
